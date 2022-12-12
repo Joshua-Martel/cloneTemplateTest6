a6d87dac7bdb6b281465aea74073a38fad456065
Piyush Rana <piyush@knoldus.com>
2022-12-12 10:19:32 -0500

    Creating user package

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 object Dependencies {
 
   object Libraries {
-	val logback-classic = "ch.qos.logback" % "logback-classic" % "1.2.3",
-	val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1",
+  val logback-classic = "ch.qos.logback" % "logback-classic" % "1.2.3",
+  val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1",
   }
 
   object Test {

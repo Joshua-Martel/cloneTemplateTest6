e9f25adf885cac149cf6b1efd92ce9768488d74f
Piyush Rana <piyush@knoldus.com>
2022-12-12 10:22:08 -0500

    Creating user package

--- build.sbt
+++ build.sbt
@@ -5,6 +5,6 @@ version := 1.0
 scalaVersion := 2.13
 
 libraryDependencies ++= Seq(
-    "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.github.pureconfig" %% "pureconfig" % "0.17.1",
 )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
 
   object Libraries {
     val logback-classic = "ch.qos.logback" % "logback-classic" % "1.2.3",
-  val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1",
+    val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1",
   }
 
   object Test {
-  
+
   }
 }
